50c2e11effdd7c730cd7cbd2ae843280c32793aa
Ondřej Kvapil <ondrej.kvapil@intel.com>
2023-04-08 19:28:31 +0200

    Fix polyglot file generation

--- .github/workflows/payload.lua
+++ .github/workflows/payload.lua
@@ -1,7 +1,9 @@
+#!/usr/bin/env lua
 print [[
 Hello there!
 
-You have in your possession the the text of my Master's thesis.
+You have in your possession the text of my Master's thesis.
 It is distributed as a polyglot file that's simultaneously
 a Lua script, a PDF document, and a ZIP archive.
-]]
+
+~ viluon]]
