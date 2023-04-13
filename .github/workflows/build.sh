cd714e66820962ca63a7f3445267040cc176aeec
Ondřej Kvapil <ondrej.kvapil@intel.com>
2023-04-13 18:05:51 +0200

    Move more dependencies into the Dockerfile

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# do not install dependencies here, APT commands belong in the Dockerfile
+# do not install dependencies here
+# APT & other setup commands belong in the Dockerfile
 
 tar --version;
 gzip --version;
@@ -11,19 +12,13 @@ latexmk -pdf ctufit-thesis.tex;
 makeglossaries ctufit-thesis;
 latexmk -pdf ctufit-thesis.tex;
 
-git clone \
-    --recursive \
-    --depth 1 \
-    --branch lua-script-hack \
-    https://github.com/viluon/truepolyglot.git;
-truepolyglot/truepolyglot pdfzip \
+/truepolyglot/truepolyglot pdfzip \
     --pdffile ctufit-thesis.pdf \
     --luafile .github/workflows/payload.lua \
     --zipfile .github/workflows/payload.zip \
     ./poly.pdf;
 chmod +x poly.pdf;
 
-wget https://www.nayuki.io/res/forcing-a-files-crc-to-any-value/forcecrc32.py;
-python3 forcecrc32.py poly.pdf 241 cafebabe;
+python3 /forcecrc32.py poly.pdf 241 cafebabe;
 
 rm -fr /tmp/* /tmp/.* truepolyglot/
